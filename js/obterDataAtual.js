f71ba7efc94f1773faec1894dc9b8a23a233440e
DWinst0n <daviwinstonsm@gmail.com>
2024-12-12 13:02:21 -0300

    Adicionar/alterar/remover itens finalizado!

---
+++ js/obterDataAtual.js
@@ -0,0 +1,10 @@
+export function obterDataAtual(locale = "pt-BR") {
+    const dataAtual = new Date();
+    const dia = dataAtual.toLocaleDateString(locale, { weekday: "long" });
+    const diaValor = dataAtual.getDate();
+    const mes = (dataAtual.getMonth() + 1).toString().padStart(2, "0");
+    const ano = dataAtual.getFullYear();
+    const horas = dataAtual.getHours().toString().padStart(2, "0");
+    const minutos = dataAtual.getMinutes().toString().padStart(2, "0");
+    return `${dia} (${diaValor}/${mes}/${ano}) às ${horas}:${minutos}`;
+};
